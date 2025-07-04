96929858920638072e43a00b6ac73725bd9c7426
AnasMunir12 <anassheik890@gmail.com>
2025-07-05 03:55:26 +0500

    Update

--- src/components/website/consulting/Consultingmain.js
+++ src/components/website/consulting/Consultingmain.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import ConsultingBanner from './ConsultingBanner'
 import  Container  from './Container'
+import Navbar from '../navbar/Navbar'
 
 function Consultingmain() {
   return (
     <>
+    <Navbar/>   
     <ConsultingBanner/>
     <Container/>
     </>
